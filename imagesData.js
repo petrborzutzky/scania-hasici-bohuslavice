1142f8b5fdf1caf09488f62b65997a817da97a08
petrborzutzky <borzutzky@gmail.com>
2023-10-26 12:34:22 +0200

    Presun casti JS kodu do imagesData.js

---
+++ imagesData.js
@@ -0,0 +1,31 @@
+export const imagesData = [
+  {
+    photo: 'images/xjqhyg9ftr.1400.jpg',
+    title: 'Scania',
+    description: 'Cisterna Scania CAS 30/8500/510 - S2R, JSDH Bohuslavice',
+  },
+
+  {
+    photo: 'images/dfx9pihfnx.1400.jpg',
+    title: 'Scania',
+    description:
+      'Vozidlo je vybaveno navijákem s tažnou silou 2.5t. Nad předním nárazníkem je na levé straně umístěna hasící lafeta. Tato je ovládána joystickem ze sedadla spolujezdce.',
+  },
+  {
+    photo: 'images/7wr4i0i8om.1400.jpg',
+    title: 'Scania - pravy bok',
+    description: 'Vozidlo aktuálně vozí 9000 litrů vody. Délka vozidla je 10m.',
+  },
+  {
+    photo: 'images/hotst0avm4.1400.jpg',
+    title: 'Scania - ze zadu',
+    description:
+      'Za výklopnými dveřmi je ukryto ovládání čerpadla. Dveře obsahují i roletu - není tedy nutno otvírat směrem nahoru celé dveře.',
+  },
+  {
+    photo: 'images/j8m12ne2zt.1400.jpg',
+    title: 'Scania - levý bok',
+    description:
+      'Cisterna obsahuje z boku 2x výsuvnou roletu. Zde je uloženo požární příslušenství. Na boku cisterny je i ledkový vodoměr signalizující množství vody v nádrži.',
+  },
+];
